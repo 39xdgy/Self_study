df7d636af5cd3c672817374ae38b97a81b46d981
39xdgy <jwang20@cornellcollege.edu>
2018-10-08 07:52:18 -0500

    Fully working

--- Room/call_cam.py
+++ Room/call_cam.py
@@ -5,10 +5,6 @@ import os
 global rval, frame
 import itchat
 
-
-    
-
-    
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(1)
 
@@ -17,11 +13,11 @@ if vc.isOpened():
 else:
     rval = False
 
-
 count_same = 0
 has_same = False
 count_diff = 0
 has_diff = False
+
 while rval:
     
     rval, now_frame = vc.read()
@@ -37,18 +33,18 @@ while rval:
     if(out == 0):
         #print("Same")
         count_same += 1
-        if (count_diff < 10):
+        if (count_diff < 25):
             count_diff = 0
     else:
         #print("Different")
         count_diff += 1
         count_same = 0
 
-    if(count_diff >= 10 and (not has_diff)):
+    if(count_diff >= 25 and (not has_diff)):
         #send_move_danger()
         has_diff = True
         cv2.imwrite("breaker.jpg", now_frame)
-    if(count_same > 100 and count_diff >= 10):
+    if(count_same > 100 and count_diff >= 25):
         #send_move_save()
         os.remove("breaker.jpg")
         count_diff = 0
@@ -59,7 +55,7 @@ while rval:
     key = cv2.waitKey(20)
     if key == 27:
         rval = False
-
-
+if(os.path.isfile("./breaker.jpg")):
+    os.remove("breaker.jpg")
 cv2.destroyWindow("preview")
 
--- Room/wechat_try.py
+++ Room/wechat_try.py
@@ -1,6 +1,6 @@
 import itchat
 import datetime, os, platform, time
-
+import cv2
 
 def send_move(friends_name, text):
     users = itchat.search_friends(name = friends_name)
@@ -22,26 +22,29 @@ def print_content(msg):
     print(msg['User']['NickName'] + 'said: ' + msg['Text'])
 
 
-
-
-
-    
 itchat.auto_login(hotReload = True)
 
 
 is_break_in = False
+key = cv2.waitKey(20)
 
-while(True):
-    if(os.path.isfile("./breaker.jpg") and (not is_break_in)):
-        is_break_in = True
-        send_move_danger()
-        itchat.send_image("breaker.jpg", toUserName = 'filehelper')
+try:
+    while(True):
+        if(os.path.isfile("./breaker.jpg") and (not is_break_in)):
+            send_move_danger()
+            itchat.send_image("breaker.jpg", toUserName = 'filehelper')
+            is_break_in = True
+        
+        
 
 
         
-    if((not os.path.isfile("./breaker.jpg")) and is_break_in):
-        send_move_save()
-        is_break_in = False
-        
+        elif((not os.path.isfile("./breaker.jpg")) and is_break_in):
+            send_move_save()
+            is_break_in = False
+except KeyboardInterrupt:
+    print("Finish")
+    
     #itchat.run()
     
+print("Finish")
